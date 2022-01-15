c94e6bc299ff0ef14a1fa1c2c3115f910250257f
Y4ROSLAV <jeros1337@gmail.com>
2022-01-15 21:09:33 +0300

    projects have been redistributed

--- src/projects.tsx
+++ src/projects.tsx
@@ -1,27 +1,26 @@
 export const projects = [
   {
     img: "https://demotivation.ru/wp-content/uploads/2020/10/1493123379_myau-krasivye-foto-malenkih-kotov-10.jpg",
-    type: "Верстка сайта",
-    name: "Data Cloud",
-    date: "Январь, 2022",
-    body: 'Впервые решил довести верстку до конца и сделать её адаптивной. Кнопки не работают, но оно и не надо.',
-    stack: "HTML, CSS, JavaScript",
-    siteUrl: "https://cloud-layout.vercel.app/",
-    githubUrl: "https://github.com/Y4R0SLAV/cloud-layout",
+    type: "Социальная сеть",
+    name: "Samurai JS",
+    date: "Ноябрь, 2021",
+    body: 'Мое изучение реакта началось с курса Samurai JS с канала IT-Kamasutra.',
+    stack: "React, Redux, JavaScript, TypeScript, HTML, CSS",
+    siteUrl: "",
+    githubUrl: "https://github.com/Y4R0SLAV/social-web",
     id: 1
   },
   {
     img: "https://demotivation.ru/wp-content/uploads/2020/10/1493123379_myau-krasivye-foto-malenkih-kotov-10.jpg",
-    type: "Портфолио",
-    name: "Ярослав",
-    date: "Декабрь, 2021",
-    body: `Решил, что нужно заявить людям о себе. Сайт вы уже видите, но код выглядит ещё лучше.`,
-    stack: "React, JavaScript, TypeScript, HTML, CSS",
-    siteUrl: "",
-    githubUrl: "https://github.com/Y4R0SLAV/my-own-portfolio",
+    type: "Интернет магазин",
+    name: "Ovsyankin",
+    date: "Ноябрь, 2021",
+    body: 'В универе сказали сделать веб приложение. Я и начал. За референс взял https://vsrap.shop. После того, как понял, что бекенд никто делать не собирается - перестал заниматься магазином.',
+    stack: "React, Redux, JavaScript, HTML, CSS",
+    siteUrl: "https://ovsyankin-boutique.herokuapp.com/",
+    githubUrl: "https://github.com/Y4R0SLAV/web-store",
     id: 2
   },
-
   {
     img: "https://demotivation.ru/wp-content/uploads/2020/10/1493123379_myau-krasivye-foto-malenkih-kotov-10.jpg",
     type: "Тестовый проект",
@@ -33,25 +32,26 @@ export const projects = [
     githubUrl: "https://github.com/Y4R0SLAV/crit-youtube-test-task",
     id: 3
   },
-{
-  img: "https://demotivation.ru/wp-content/uploads/2020/10/1493123379_myau-krasivye-foto-malenkih-kotov-10.jpg",
-  type: "Интернет магазин",
-  name: "Ovsyankin",
-  date: "Ноябрь, 2021",
-  body: 'В универе сказали сделать веб приложение. Я и начал. За референс взял https://vsrap.shop. После того, как понял, что бекенд никто делать не собирается - перестал заниматься магазином.',
-  stack: "React, Redux, JavaScript, HTML, CSS",
-  siteUrl: "https://ovsyankin-boutique.herokuapp.com/",
-  githubUrl: "https://github.com/Y4R0SLAV/web-store",
-  id: 4
-},
-{
-  img: "https://demotivation.ru/wp-content/uploads/2020/10/1493123379_myau-krasivye-foto-malenkih-kotov-10.jpg",
-  type: "Социальная сеть",
-  name: "Samurai JS",
-  date: "Ноябрь, 2021",
-  body: 'Мое изучение реакта началось с курса Samurai JS с канала IT-Kamasutra.',
-  stack: "React, Redux, JavaScript, TypeScript, HTML, CSS",
-  siteUrl: "",
-  githubUrl: "https://github.com/Y4R0SLAV/social-web",
-  id: 5
-}]
+  {
+    img: "https://demotivation.ru/wp-content/uploads/2020/10/1493123379_myau-krasivye-foto-malenkih-kotov-10.jpg",
+    type: "Портфолио",
+    name: "Ярослав",
+    date: "Декабрь, 2021",
+    body: `Решил, что нужно заявить людям о себе. Сайт вы уже видите, но код выглядит ещё лучше.`,
+    stack: "React, JavaScript, TypeScript, HTML, CSS",
+    siteUrl: "",
+    githubUrl: "https://github.com/Y4R0SLAV/my-own-portfolio",
+    id: 4
+  },
+  {
+    img: "https://demotivation.ru/wp-content/uploads/2020/10/1493123379_myau-krasivye-foto-malenkih-kotov-10.jpg",
+    type: "Верстка сайта",
+    name: "Data Cloud",
+    date: "Январь, 2022",
+    body: 'Впервые решил довести верстку до конца и сделать её адаптивной. Кнопки не работают, но оно и не надо.',
+    stack: "HTML, CSS, JavaScript",
+    siteUrl: "https://cloud-layout.vercel.app/",
+    githubUrl: "https://github.com/Y4R0SLAV/cloud-layout",
+    id: 5
+  },
+]
